1729c8d45a0212793a5a26b3c41f07f16a51e344
Emilien PAUL <emilienpaul@hotmail.fr>
2024-04-14 15:13:00 +0200

    init symfony

---
+++ src/Entity/Article.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ApiResource]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Contenu = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $DatePublication = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Auteur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): static
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->Contenu;
+    }
+
+    public function setContenu(string $Contenu): static
+    {
+        $this->Contenu = $Contenu;
+
+        return $this;
+    }
+
+    public function getDatePublication(): ?\DateTimeInterface
+    {
+        return $this->DatePublication;
+    }
+
+    public function setDatePublication(\DateTimeInterface $DatePublication): static
+    {
+        $this->DatePublication = $DatePublication;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->Auteur;
+    }
+
+    public function setAuteur(string $Auteur): static
+    {
+        $this->Auteur = $Auteur;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Image.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+#[ApiResource]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Path = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->Path;
+    }
+
+    public function setPath(string $Path): static
+    {
+        $this->Path = $Path;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+}
